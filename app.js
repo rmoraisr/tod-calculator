6550d3c99d62409d781eaf8527cd5c961d91ac4a
rflmorais <rflmorais@gmail.com>
2022-07-21 15:03:06 -0300

    Create a new function operate() to take cares of the math

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const keys = document.querySelector('.calculator-keys');
 
-// Listening to key click
+/**
+ * Given two numbers and an operator, return the result of the operation.
+ * @param num1 - The first number to operate on.
+ * @param num2 - The second number to operate on.
+ * @param operator - The operator to use.
+ * @returns the result of the operation.
+ * @call operate(2, 4, 'add'); // 6
+ */
+function operate(num1, num2, operator) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    const actions = {
+        add: (a, b) => a + b,
+        subtract: (a, b) => a - b,
+        multiply: (a, b) => a * b,
+        divide: (a, b) => a / b,
+    };
+
+    return actions[operator](num1, num2);
+}
+
+/* Listening to the click event on the keys. */
 keys.addEventListener('click', (e) => {
     // aliases to make life easier
     const key = e.target;
