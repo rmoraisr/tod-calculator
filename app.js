6872af1e106feb76660167435da3d1e48d86b553
rflmorais <rflmorais@gmail.com>
2022-07-22 16:22:12 -0300

    Moved all others actions that changes display.textContent to the createResultString

--- app.js
+++ app.js
@@ -34,85 +34,82 @@ keys.addEventListener('click', (e) => {
     const action = key.dataset.action; // custom attribute created in the html
     const currentDisplayNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType; // custom data attribute of the calculator class
+});
 
+const createResultString = () => {
+    //! VAriables required
+    // 1. key
+    // 2. keyContent
+    // 3. currentDisplayNum
+    // 4. previousKeyType
+    // 5. action
+    // 6. calculator.dataset.firstNum
+    // 7. calculator.dataset.operator
+    // 8. calculator.dataset.secondNum
 
-
-    const createResultString = () => {
-        // VAriables required
-        // 1. key
-        // 2. keyContent
-        // 3. currentDisplayNum
-        // 4. previousKeyType
-        // 5. action
-        // 6. calculator.dataset.firstNum
-        // 7. calculator.dataset.operator
-
-        /* Checking if the key pressed is a number. If it is, it checks if the current display number
+    /* Checking if the key pressed is a number. If it is, it checks if the current display number
         is 0, or if the previous key type was an operator or calculate. If it is, it returns the key
         content. Otherwise, it returns the current display number plus the key content. */
-        if (key.className === 'key-number') {
-            return currentDisplayNum === '0' ||
-                previousKeyType === 'operator' ||
-                previousKeyType === 'calculate'
-                ? keyContent
-                : currentDisplayNum + keyContent;
+    if (key.className === 'key-number') {
+        return currentDisplayNum === '0' ||
+            previousKeyType === 'operator' ||
+            previousKeyType === 'calculate'
+            ? keyContent
+            : currentDisplayNum + keyContent;
+    }
 
-            //TODO: add to calculator state: "calculator.dataset.previousKeyType = 'number';""
-        }
-
-        /* Checking if the previous key type was an operator or calculate, and if so, it returns 0.
+    /* Checking if the previous key type was an operator or calculate, and if so, it returns 0.
         Otherwise, it checks if the current display number includes a decimal point. If it does, it
         returns the current display number. Otherwise, it returns the current display number plus a
         decimal point. */
-        if (action === 'decimal') {
-            if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
-            if (!currentDisplayNum.includes('.')) return currentDisplayNum + '.'
-            return currentDisplayNum // return when neither conditions are matched. Otherwise createResultString would return undefined
+    if (action === 'decimal') {
+        if (previousKeyType === 'operator' || previousKeyType === 'calculate')
+            return '0.';
+        if (!currentDisplayNum.includes('.')) return currentDisplayNum + '.';
+        return currentDisplayNum; // return when neither conditions are matched. Otherwise createResultString would return undefined
+    }
 
-            //TODO: add to calculator state: "calculator.dataset.previousKeyType = 'decimal';""
-        }
+    if (key.className === 'key-operator') {
+        const firstNum = calculator.dataset.firstNum; //TODO: move to a new function
+        const operator = calculator.dataset.operator; // TODO: move to a new function
 
-        if (key.className === 'key-operator') {
-            const firstNum = calculator.dataset.firstNum;
-            const operator = calculator.dataset.operator;
-
-            return firstNum &&
+        return firstNum &&
             operator &&
             previousKeyType !== 'operator' &&
             previousKeyType !== 'calculate'
             ? operate(firstNum, currentDisplayNum, operator)
-            : currentDisplayNum
-        }
-    };
-
-
-    } else if (action === 'clear') {
-        display.textContent = '0';
-        calculator.dataset.firstNum = '';
-        calculator.dataset.secondNum = '';
-        calculator.dataset.operator = '';
-        calculator.dataset.previousKeyType = 'clear';
-    } else if (action === 'changeSign') {
-        display.textContent = currentDisplayNum * -1;
-        calculator.dataset.firstNum = currentDisplayNum;
-        calculator.dataset.previousKeyType = 'changeSign';
-    } else if (action === 'delete') {
-        if (currentDisplayNum !== '0' && previousKeyType === 'number') {
-            if (currentDisplayNum.length > 1) {
-                display.textContent = currentDisplayNum.slice(0, -1);
-            } else if (currentDisplayNum.length === 1) {
-                display.textContent = '0';
-            }
-        } else if (action === 'calculate') {
-        if (firstNum) {
-            if (previousKeyType === 'calculate') {
-                firstNum = currentDisplayNum;
-                secondNum = calculator.dataset.secondNum;
-            }
-            // perform a calculation just after a number is clicked
-            display.textContent = operate(firstNum, secondNum, operator);
-        }
-        calculator.dataset.secondNum = secondNum;
-        calculator.dataset.previousKeyType = 'calculate';
+            : currentDisplayNum;
     }
-});
+
+    if (action === 'clear') return 0;
+
+    if (action === 'calculate') {
+        const firstNum = calculator.dataset.firstNum;
+        const operator = calculator.dataset.operator;
+        const secondNum = calculator.dataset.secondNum;
+
+        if (firstNum) {
+            return previousKeyType === 'calculate'
+                ? operate(firstNum, secondNum, operator)
+                : operate(firstNum, currentDisplayNum, operator);
+        } else {
+            return currentDisplayNum;
+        }
+    }
+
+    if (action === 'changeSign') {
+        currentDisplayNum = currentDisplayNum * -1;
+        calculator.dataset.firstNum = currentDisplayNum;
+        return currentDisplayNum;
+    }
+
+    if (action === 'delete') {
+        return currentDisplayNum !== '0' &&
+            (previousKeyType === 'number' || previousKeyType === 'decimal') &&
+            currentDisplayNum.length > 1
+            ? currentDisplayNum.slice(0, -1)
+            : currentDisplayNum.length < 1
+            ? '0'
+            : currentDisplayNum;
+    }
+};
